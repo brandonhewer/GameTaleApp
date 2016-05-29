8569a5e1d8515eaef5d5149915c6d58447d21f2b
Brandon Hewer <brandonhewer@138-38-157-245.eduroam.bath.ac.uk>
2016-05-29 10:07:20 +0100

    Mate

---
+++ Assets/Move.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+	
+	private float movementSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		MoveControl();
+	}
+
+	// Moves the object based on keyboard input.
+	void MoveControl() {
+		var x = Input.GetAxis("Horizontal") * movementSpeed;
+		var z = Input.GetAxis("Vertical") * movementSpeed;
+
+		transform.Translate(x, 0, z);
+	}
+}

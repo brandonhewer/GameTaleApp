0db405f7d7877f0e1defba63ab5428e4014899e6
Brandon Hewer <brandonhewer@138-38-157-245.eduroam.bath.ac.uk>
2016-05-29 10:31:18 +0100

    Stairs

--- Assets/LoadGame.cs
+++ Assets/LoadGame.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class LoadGame : MonoBehaviour {
 
+	public int height;
+	public int width;
+	public GameObject floor;
+
 	// Use this for initialization
 	void Start () {
-		
+		drawStairs();
 	}
 	
 	// Update is called once per frame
@@ -13,11 +17,34 @@ public class LoadGame : MonoBehaviour {
 	
 	}
 
+	/**
+	 * Draws the stairs. 
+	 */
 	void drawStairs() {
 
+		// Iterate through all stair levels
+		// to draw.
+		for(int i=0; i<height; i++) {
+			drawStairLevel (i);
+		}
 	}
 
-	void drawStairLevel() {
+	/**
+	 * Draws a stair level.
+	 * 
+	 * level:	The height of the stair level.
+	 */
+	void drawStairLevel(int height) {
 
+		// Iterate through all stair components
+		// on the current level to draw.
+		for(int i=0; i<width; i++) {
+			createStairComponent (height, i);
+		}
+	}
+
+	void createStairComponent(int height, int width) {
+		Vector3 pos = new Vector3(width, height, height);
+		GameObject component = (GameObject) Instantiate(floor, pos, Quaternion.identity);
 	}
 }
